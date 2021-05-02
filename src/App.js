d6c063b4951cfd26226ca14a8721a904454a411c
whoadarshpandya <adarshpandya0@gmail.com>
2021-05-02 18:23:53 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <span className="navbar-brand">koi to hospital</span>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mb-2 mb-lg-0 d-flex ms-auto">
+              <li className="nav-item mx-3">Register </li>
+              <li className="nav-item mx-3">Stats</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <p>about</p>
+            </Route>
+            <Route path="/users">
+              <p>user</p>
+            </Route>
+            <Route path="/">
+              <p>home</p>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
